a2c6b44d53d9decdbab4acb7f4663dc857d8db15
vitran <tranuytrieuvi@protonmail.com>
2020-09-29 13:32:30 +0700

    back up

--- learnJs/js/script.js
+++ learnJs/js/script.js
@@ -46,3 +46,57 @@
 //     console.log("x == y");
 // else
 //     console.log("x != y");
+
+// ALL TRUE
+// if (true &&
+//     "hello" &&
+//     1 &&
+//     -1 &&
+//     "false") {
+//     console.log("ALL TRUE");
+// }
+
+// function a()
+// {
+//     // INVALID because after 'return' there is no word -> JS VM will assume that there is nothing to return -> undefined
+//     return
+//     {
+//         name: "Chris"
+//     };
+// }
+
+// function b() {
+//     return {
+//         name: "Chris"
+//     };
+// }
+
+// console.log(a());
+// console.log(b());
+
+// For loop
+// var sum = 0;
+// for (var i = 0; i < 10; i++) {
+//     sum = sum + i;
+// }
+
+// console.log("sum from 0 to 9 is: " + sum);
+
+// Default value
+// function orderChickenWith(sideDish) {
+//     // if (sideDish === undefined)
+//     //     sideDish = "whatever!";
+//     sideDish = sideDish || "whatever!"; // Return the value that is TRUE
+//     console.log("Chicken with " + sideDish);
+// }
+
+// orderChickenWith("noddles");
+// orderChickenWith();
+
+// var company = new Object();
+// company.name = "Facebook";
+// company.ceo = new Object();
+// company.ceo.firstName = "Mark"; // INVALID! ceo not defined
+// company.ceo.favColor = "blue";
+
+// console.log(company);
