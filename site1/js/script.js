9c8b0b4fec9c768ab1096c73ba7d67f2cb2a039c
Chris <tranuytrieuvi@protonmail.com>
2020-10-05 23:40:00 +0700

    lecture 59 back up

--- site1/js/script.js
+++ site1/js/script.js
@@ -1,1 +1,11 @@
-// PLACE HOLDER
+$(function() { // Same as document.addEventListener("DOMContentLoaded"...)
+
+    // Same as document.querySelector("#navbarToggle").addEventListener("blur",...)
+    $("#navbar-toggle").blur(function(event) {
+            var screenWidth = window.innerWidth;
+            if (screenWidth < 768)
+                $("#collapsable-nav").collapse('hide');
+        }
+    )
+
+});
