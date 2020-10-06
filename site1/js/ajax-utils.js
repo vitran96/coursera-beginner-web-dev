302ba61526aa55562ad06b3d1788f51f9e56d964
Chris <tranuytrieuvi@protonmail.com>
2020-10-06 22:35:47 +0700

    lecture 60 part 1

---
+++ site1/js/ajax-utils.js
@@ -0,0 +1,61 @@
+(function(global) {
+
+// Set up a namespace for utility
+var ajaxUtils = {};
+
+// Return an HTTP request obj
+function getRequestObj() {
+    if (window.XMLHttpRequest) {
+        return new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        return new ActiveXObject("Microsoft.XMLHTTP");
+    } else {
+        global.alert("Ajax is not supported!");
+        return null;
+    }
+}
+
+ajaxUtils.sendGetRequest =
+    function(
+        requestUrl,
+        responseHandler,
+        isJsonResponse
+    ) {
+        var request = getRequestObj();
+        request.onreadystatechange =
+            function() {
+                handleResponse(
+                    request,
+                    responseHandler,
+                    isJsonResponse
+                );
+            };
+        request.open("GET", requestUrl, true);
+        request.send(null); // For POST only
+    };
+
+// Only calls user provided 'responseHandler'
+// function if response is ready
+// and not an error
+function handleResponse(
+    request,
+    responseHandler,
+    isJsonResponse
+) {
+    if (request.readyState == 4 &&
+        request.status == 200
+    ) {
+        if (isJsonResponse == undefined)
+            isJsonResponse = true;
+
+        if (isJsonResponse)
+            responseHandler(JSON.parse(request.responseText));
+        else
+        responseHandler(request.responseText);
+    }
+}
+
+// Expose utility to the global object
+global.$ajaxUtils = ajaxUtils;
+
+})(window);
--- site1/js/script.js
+++ site1/js/script.js
@@ -9,7 +9,39 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...)
         }
     )
 
-    $("#navbarToggle").click(function (event) {
-        $(event.target).focus();
-    });
+    // $("#navbarToggle").click(function (event) {
+    //     $(event.target).focus();
+    // });
 });
+
+(function(global) {
+    var dc = {};
+
+    var homeHtml = "snippets/home-snippet.html";
+
+    // Convinience function for inserting innerHTML for 'select'
+    var insertHtml = function(selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHtml = html;
+    }
+
+    var showLoading = function(selector) {
+        var html = "<div class='text-center'>";
+        html += "<img src='images/ajax-loader.gif'></div>";
+        insertHtml(selector, html);
+    }
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(
+            homeHtml,
+            function(responseText) {
+                document.querySelector("#main-content")
+                    .innerHTML = responseText;
+            },
+            false
+        );
+    })
+
+    global.$dc = dc;
+})(window);
