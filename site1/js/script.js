4d4a2e7c661f7596429661291dc25569761faef5
Chris <tranuytrieuvi@protonmail.com>
2020-10-08 23:43:11 +0700

    back up

--- site1/js/script.js
+++ site1/js/script.js
@@ -10,7 +10,7 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...)
     )
 
     // $("#navbarToggle").click(function (event) {
-    //     $(event.target).focus();
+    //     $(event.target).fo2cus();
     // });
 });
 
@@ -18,6 +18,9 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...)
     var dc = {};
 
     var homeHtml = "snippets/home-snippet.html";
+    var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+    var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+    var categoryHtml = "snippets/category-snippet.html";
 
     // Convinience function for inserting innerHTML for 'select'
     var insertHtml = function(selector, html) {
@@ -31,6 +34,17 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...)
         insertHtml(selector, html);
     }
 
+    var insertProperty = function(
+        string,
+        propName,
+        propValue
+    ) {
+        var propToReplace = "{{" + propName + "}}";
+        string = string.replace(new RegExp(propToReplace, "g"), propValue);
+
+        return string;
+    };
+
     document.addEventListener("DOMContentLoaded", function(event) {
         showLoading("#main-content");
         $ajaxUtils.sendGetRequest(
@@ -42,6 +56,62 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...)
             false
         );
     })
+
+    dc.loadMenuCategories = function() {
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(
+            allCategoriesUrl,
+            buildAndShowCategoriesHtml,
+            true
+        )
+    };
+
+    // Build HTML for the categories page based on the data from the server
+    function buildAndShowCategoriesHtml(categories) {
+        // Load title snippet of categories page
+        $ajaxUtils.sendGetRequest(
+            categoriesTitleHtml,
+            function(categoriesTitleHtml) {
+                // Retrieve single category snippet
+                $ajaxUtils.sendGetRequest(
+                    categoryHtml,
+                    function(categoryHtml) {
+                        var categoriesViewHtml = buildAndShowCategoriesViewHtml(
+                            categories,
+                            categoriesTitleHtml,
+                            categoryHtml
+                        );
+
+                        insertHtml("#main-content", categoriesViewHtml);
+                    },
+                    false
+                );
+            },
+            false
+        )
+    }
+
+    function buildAndShowCategoriesViewHtml(
+        categories,
+        categoriesTitleHtml,
+        categoryhtml
+    ) {
+        var finalHtml = categoriesTitleHtml;
+        finalHtml += '<section class="row">';
+
+        for (var i = 0; i < categories.length; i++) {
+            // Insert category values
+            var html = categoryHtml;
+            var name = "" + categories[i].name;
+            var short_name = categories[i].short_name;
+            html = insertProperty(html, "name", name);
+            html = insertProperty(html, "short_name", short_name);
+            finalHtml += html;
+        }
+
+        finalHtml += "</section>";
+        return finalHtml;
+    }
 
     global.$dc = dc;
 })(window);
